0d2fbfda334f8584f33fe6dbb9c7c1a9095c1fb5
rokemagik <emidqtrabajo@gmail.com>
2025-10-06 00:17:28 -0300

    Arreglando errores

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -2,7 +2,7 @@
 function CartWidget() {
     return (
         <div className="icono_div">
-            <i class="bi bi-cart2"></i>
+            <i className="bi bi-cart2"></i>
         </div>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,18 +12,18 @@ function NavBar() {
                         type="text"
                         placeholder="Buscar productos"
                     />
-                    <button className="boton"><i class="bi bi-search"></i></button>
+                    <button className="boton"><i className="bi bi-search"></i></button>
                 </div>
                 <nav className="navegacion">
                     <a href="">Inicio</a>
-                    <a href=""><i class="bi bi-bag-fill"></i> Productos</a>
-                    <a href=""><i class="bi bi-person-fill"></i> Contacto</a>
+                    <a href=""><i className="bi bi-bag-fill"></i> Productos</a>
+                    <a href=""><i className="bi bi-person-fill"></i> Contacto</a>
                     <CartWidget />
                 </nav>
             </div>
             <div className="contenedor_barra-secundario">
                 <nav className="navegacion">
-                    <a href=""><i class="bi bi-chevron-down"></i> CATEGORIAS</a>
+                    <a href=""><i className="bi bi-chevron-down"></i> CATEGORIAS</a>
                     <a href="">Pc de escritorio</a>
                     <a href="">Notebooks</a>
                     <a href="">Monitores</a>
